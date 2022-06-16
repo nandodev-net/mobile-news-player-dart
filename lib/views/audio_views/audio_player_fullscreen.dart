5305c8f175b31a61bd1fb9947a9b035bcaa278e6
Fernando Gonzalez <gonzalez0fer@gmail.com>
2022-06-16 01:24:13 -0400

    music player full screen buttons and player slider

--- lib/views/audio_views/audio_player_fullscreen.dart
+++ lib/views/audio_views/audio_player_fullscreen.dart
@@ -12,6 +12,7 @@ class PlayerScreen extends StatefulWidget {
 class _PlayerScreenState extends State<PlayerScreen> {
   late ScrollController scrollController;
   double imageOpacity = 1;
+  double _currentSliderValue = 20;
 
   @override
   Widget build(BuildContext context) {
@@ -23,7 +24,17 @@ class _PlayerScreenState extends State<PlayerScreen> {
             height: 500.0,
             width: MediaQuery.of(context).size.width,
             alignment: Alignment.center,
-            color: Color.fromARGB(255, 30, 233, 206),
+                                decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [
+                          Colors.green.withOpacity(1),
+                          Colors.white.withOpacity(0.8),
+                          Colors.white.withOpacity(0.9),
+                        ],
+                      ),
+                    ),
             child: Wrap(children: [
               Column(
                 mainAxisAlignment: MainAxisAlignment.center,
@@ -83,35 +94,120 @@ class _PlayerScreenState extends State<PlayerScreen> {
                           const SizedBox(height: 272.0),
                           Padding(
                             padding: const EdgeInsets.all(16.0),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                const SizedBox(height: 10),
-                                Center(
-                                  child: Text(
-                                    widget.audio.title,
-                                    style: Theme.of(context)
-                                        .textTheme
-                                        .bodyText1
-                                        ?.copyWith(fontSize: 17.0),
+                            child: Padding(
+                              padding:
+                                  const EdgeInsets.only(left: 10, right: 10),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Column(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceEvenly,
+                                    children: [
+                                      SizedBox(
+                                        height: 30.0,
+                                      ),
+                                      Text(
+                                        widget.audio.title,
+                                        style: TextStyle(
+                                            fontSize: 18,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                      Container(
+                                        width: 150,
+                                        child: Text(
+                                          widget.audio.author,
+                                          maxLines: 1,
+                                          textAlign: TextAlign.center,
+                                          style: TextStyle(
+                                              fontSize: 15,
+                                              color: Colors.black
+                                                  .withOpacity(0.5)),
+                                        ),
+                                      ),
+                                    ],
                                   ),
-                                ),
-                                const SizedBox(height: 10),
-                                Center(
-                                  child: Text(
-                                    widget.audio.author,
-                                    style: Theme.of(context)
-                                        .textTheme
-                                        .bodyText1
-                                        ?.copyWith(fontSize: 17.0),
-                                  ),
-                                ),
-                                const SizedBox(height: 120),
-                              ],
+                                ],
+                              ),
                             ),
-                          )
+                          ),
                         ],
                       ),
+                    ),
+                  ),
+                  Slider(
+                      activeColor: Colors.green,
+                      value: _currentSliderValue,
+                      min: 0,
+                      max: 200,
+                      onChanged: (value) {
+                        setState(() {
+                          _currentSliderValue = value;
+                        });
+                        //seekSound();
+                      }),
+                  SizedBox(
+                    height: 2,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 30, right: 30),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "1:50",
+                          style:
+                              TextStyle(color: Colors.black.withOpacity(0.5)),
+                        ),
+                        Text(
+                          "4:68",
+                          style:
+                              TextStyle(color: Colors.black.withOpacity(0.5)),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 2,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20, right: 20),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        IconButton(
+                            icon: Icon(
+                              Icons.skip_previous,
+                              color: Colors.black.withOpacity(0.8),
+                              size: 25,
+                            ),
+                            onPressed: null),
+                        IconButton(
+                            iconSize: 50,
+                            icon: Container(
+                              decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color: Colors.green),
+                              child: Center(
+                                child: Icon(
+                                  Icons.play_arrow,
+                                  // isPlaying
+                                  //     ? Entypo.controller_stop
+                                  //     : Entypo.controller_play,
+                                  size: 28,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                            onPressed: () {}),
+                        IconButton(
+                            icon: Icon(
+                              Icons.skip_next,
+                              color: Colors.black.withOpacity(0.8),
+                              size: 25,
+                            ),
+                            onPressed: null),
+                      ],
                     ),
                   ),
                 ],
